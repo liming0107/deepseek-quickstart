62529aa889cd34626c3a3131d9d1bb075a6e9bc4
黎明 <3150996045@qq.com>
2025-07-07 11:14:22 +0800

    第四章作业

---
+++ homework/rag_milvus_deepseek_mfd.py
@@ -0,0 +1,96 @@
+import os
+
+# api_key = os.getenv("OPENAI_API_KEY")
+from glob import glob
+
+text_lines = []
+
+for file_path in glob("../deepseek/api/mfd.md", recursive=True):
+    with open(file_path, "r") as file:
+        file_text = file.read()
+
+    text_lines += file_text.split("# ")
+
+len(text_lines)
+
+from openai import OpenAI
+
+deepseek_client = OpenAI(
+    api_key="sk-11d98c39942d450c8ba49526a76ec0c9",
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # DeepSeek API 的基地址
+)
+
+from pymilvus import model as milvus_model
+
+embedding_model = milvus_model.DefaultEmbeddingFunction()
+
+from pymilvus import MilvusClient
+
+milvus_client = MilvusClient(uri="http://192.168.5.24:19530")
+
+collection_name = "default"
+test_embedding = embedding_model.encode_queries(["This is a test"])[0]
+embedding_dim = len(test_embedding)
+milvus_client.create_collection(
+    collection_name=collection_name,
+    dimension=embedding_dim,
+    metric_type="IP",  # 内积距离
+    consistency_level="Strong",  # 支持的值为 (`"Strong"`, `"Session"`, `"Bounded"`, `"Eventually"`)。更多详情请参见 https://milvus.io/docs/consistency.md#Consistency-Level。
+)
+
+from tqdm import tqdm
+
+data = []
+
+doc_embeddings = embedding_model.encode_documents(text_lines)
+question = "登记机构应当履行哪些职责?"
+
+for i, line in enumerate(tqdm(text_lines, desc="Creating embeddings")):
+    data.append({"id": i, "vector": doc_embeddings[i], "text": line})
+
+milvus_client.insert(collection_name=collection_name, data=data)
+
+search_res = milvus_client.search(
+    collection_name=collection_name,
+    data=embedding_model.encode_queries(
+        [question]
+    ),  # 将问题转换为嵌入向量
+    limit=3,  # 返回前3个结果
+    search_params={"metric_type": "IP", "params": {}},  # 内积距离
+    output_fields=["text"],  # 返回 text 字段
+)
+
+import json
+
+retrieved_lines_with_distances = [
+    (res["entity"]["text"], res["distance"]) for res in search_res[0]
+]
+print(json.dumps(retrieved_lines_with_distances, indent=4))
+
+context = "\n".join(
+    [line_with_distance[0] for line_with_distance in retrieved_lines_with_distances]
+)
+
+SYSTEM_PROMPT = """
+Human: 你是一个 AI 助手。你能够从提供的上下文段落片段中找到问题的答案。
+"""
+USER_PROMPT = f"""
+请使用以下用 <context> 标签括起来的信息片段来回答用 <question> 标签括起来的问题。最后追加原始回答的中文翻译，并用 <translated>和</translated> 标签标注。
+<context>
+{context}
+</context>
+<question>
+{question}
+</question>
+<translated>
+</translated>
+"""
+
+response = deepseek_client.chat.completions.create(
+    model="deepseek-v3",
+    messages=[
+        {"role": "system", "content": SYSTEM_PROMPT},
+        {"role": "user", "content": USER_PROMPT},
+    ],
+)
+print(response.choices[0].message.content)
